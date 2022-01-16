ef1d22a9e6ef634efd4ea91b04dd665f2cab1373
Thyago Ribeiro <thyago.ribeiro@webjump.com.br>
2022-01-16 05:46:18 -0400

    Created component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+export function Header(){
+    return(
+        <header>
+            <h1>logo</h1>
+
+            <nav>
+                <a href="#">link 01</a>
+                <a href="#">link 02</a>
+                <a href="#">link 03</a>
+                <a href="#">link 04</a>
+            </nav>
+        </header>
+    )
+}
